9fa6ffe11221ec11418c5c6208f01b2dd99eee52
Fakhreddine Farik <donnmoreno400@gmail.com>
2022-09-23 13:18:28 +0200

    humberger-solution

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let menu = document.querySelector("#menu");
 let menuBar = document.querySelector("#menu-bar");
 
 menuBar.addEventListener("click", () => menu.classList.toggle("menu-toggle"));
+console.log("hola");
